0088937c84d857ffe62d19759a43bd51711b2b58
Helitonsf <gerenteheliton@gmail.com>
2026-09-09 09:10:20 -0300

    chore: remove legacy apps tree

--- apps/api/src/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
-import { Green15kModule } from './green15k/green15k.module';
-
-@Module({ imports: [Green15kModule], controllers: [HealthController] })
-export class AppModule {}
